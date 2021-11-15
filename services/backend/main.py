f4102ec05823110efc4ab0e2094fa55d1e9c8870
Evgeniy Morozov <emorozov1101@gmail.com>
2021-11-15 16:03:14 +0500

    test message

---
+++ services/backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from uvicorn import run
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    return {"message": "Test message"}
+
+
+if __name__ == "__main__":
+    run("main:app", port=5000, host="0.0.0.0", reload=True)
