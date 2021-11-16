0e184c7a9bca4420f89c47363ed5f200be2cfab5
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-16 20:29:40 +0500

    prepare

--- services/backend/db/base.py
+++ services/backend/db/base.py
@@ -5,4 +5,4 @@ from core.config import DATABASE_URL
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
