0dc1db30443fc3404dd4300761af314e776ae87c
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-17 11:01:23 +0500

    create async db engine

--- services/backend/db/base.py
+++ services/backend/db/base.py
@@ -1,8 +1,10 @@
 from databases import Database
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.asyncio import create_async_engine
 from core.config import DATABASE_URL
 
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
-engine = create_engine(DATABASE_URL, echo=True)
+# engine = create_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
