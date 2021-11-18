91b1e6fcd99ba7e7a396d739ea5a74e6170a0882
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-18 16:16:30 +0500

    commit

--- services/backend/db/users.py
+++ services/backend/db/users.py
@@ -1,20 +1,8 @@
 from datetime import datetime as dt
 
-from sqlalchemy import Table, Column, Integer, String, DateTime
-
-from db.base import metadata
+from db.base import Base
 
 
-user = Table(
-    "users",
-    metadata,
-    Column("id", Integer, primary_key=True, unique=True, autoincrement=True, nullable=False),
-    Column("username", String, unique=True, nullable=False),
-    Column("email", String, unique=True, nullable=False),
-    Column("hashed_password", String),
-    Column("created_at", DateTime, nullable=False, default=dt.utcnow()),
-    Column("updated_at", DateTime, nullable=False, onupdate=dt.utcnow()),
-    Column("bio", String, nullable=True),
-    Column("image", String, nullable=True),
-    Column("bio", String),
-)
+class User(Base):
+    __tablename__ = "users"
+    username = Column()
