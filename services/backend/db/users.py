eccaadd317bc96eed6dad22c02f9e3872941760a
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-18 20:52:09 +0500

    add User  db model

--- services/backend/db/users.py
+++ services/backend/db/users.py
@@ -1,10 +1,14 @@
-import sqlalchemy
-from .base import metadata
-import datetime
+from tortoise.fields import IntField, CharField, DatetimeField
+from tortoise.models import Model
 
 
-users = sqlalchemy.Table(
-    "users",
-    metadata,
-    
-)
+class User(Model):
+    id = IntField(pk=True)
+    username = CharField(max_length=80, unique=True, null=False)
+    email = CharField(max_length=100, unique=True, null=False)
+    password = CharField(max_length=128, null=True)
+    created_at = DatetimeField(auto_now_add=True)
+    updated_at = DatetimeField(auto_now=True)
+    bio = CharField(max_length=300, null=True)
+    image = CharField(max_length=120, null=True)
+
