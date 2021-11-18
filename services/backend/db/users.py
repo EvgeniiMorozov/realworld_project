370a03377462aacfdc1edcff33de6760ddcfee73
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-18 21:08:38 +0500

    add user db-model

--- services/backend/db/users.py
+++ services/backend/db/users.py
@@ -1,14 +1,11 @@
-from tortoise.fields import IntField, CharField, DatetimeField
-from tortoise.models import Model
+from tortoise.fields import CharField
+
+from db.database import BaseModel, TimestampMixin
 
 
-class User(Model):
-    id = IntField(pk=True)
+class User(TimestampMixin, BaseModel):
     username = CharField(max_length=80, unique=True, null=False)
     email = CharField(max_length=100, unique=True, null=False)
     password = CharField(max_length=128, null=True)
-    created_at = DatetimeField(auto_now_add=True)
-    updated_at = DatetimeField(auto_now=True)
     bio = CharField(max_length=300, null=True)
     image = CharField(max_length=120, null=True)
-
