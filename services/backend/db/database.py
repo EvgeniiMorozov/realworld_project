df5fa839d0724abff792ddbdc08d1b8f3060480e
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-18 21:08:12 +0500

    add timestamp mixin and base-model

--- services/backend/db/database.py
+++ services/backend/db/database.py
@@ -1,6 +1,15 @@
-from tortoise.fields import IntField
+from tortoise.fields import DatetimeField, IntField
 from tortoise.models import Model
 
 
-class PKModel(Model):
+class TimestampMixin:
+    created_at = DatetimeField(auto_now_add=True)
+    updated_at = DatetimeField(auto_now=True)
+
+
+class BaseModel(Model):
     id = IntField(pk=True)
+
+    class Meta:
+        abstract = True
+        # table = cls.__name__.lower()
