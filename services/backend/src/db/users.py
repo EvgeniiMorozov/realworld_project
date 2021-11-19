03c15627bba918c0ec374ffd61b7c6a6f3a6c430
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-19 16:41:56 +0500

    add user model for db

--- services/backend/src/db/users.py
+++ services/backend/src/db/users.py
@@ -1,6 +1,6 @@
 from datetime import datetime as dt
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime
 
 from db.base import Base
 
@@ -8,6 +8,11 @@ from db.base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     username = Column(String(80), unique=True, nullable=False)
     email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(128), nullable=True)
+    created_at = Column(DateTime, nullable=False, default=dt.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=dt.utcnow(), onupdate=dt.utcnow())
+    bio = Column(String(300), nullable=True)
+    image = Column(String(120), nullable=True)
