deb7ac415f4f5fd686bce22165157341d94a995a
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-19 20:37:34 +0500

    commit

--- services/backend/src/db/profile.py
+++ services/backend/src/db/profile.py
@@ -1,19 +1,13 @@
 from sqlalchemy import Column, ForeignKey, Integer
-from sqlalchemy.orm import relationship
 
 from db.base import Base
 from db.database import reference_column
-from db.users import User
 
 
-class Followers(Base):
-    __tablename__ = "followers_assoc"
-    follower = Column(Integer, ForeignKey("userprofile.user_id"))
-    followed_by = Column(Integer, ForeignKey("userprofile.user_id"))
+
 
 
 class UserProfile(Base):
     __tablename__ = "userprofile"
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     user_id = reference_column("users", nullable=False)
-
