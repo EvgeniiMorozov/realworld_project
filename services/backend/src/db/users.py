745638f5e05a534b20442722f3eabcbb2ad6e66a
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-19 20:40:09 +0500

    modify user db-model, add followers db-model

--- services/backend/src/db/users.py
+++ services/backend/src/db/users.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 from db.base import Base
 
@@ -16,3 +17,33 @@ class User(Base):
     updated_at = Column(DateTime, nullable=False, default=dt.utcnow(), onupdate=dt.utcnow())
     bio = Column(String(300), nullable=True)
     image = Column(String(120), nullable=True)
+    token = Column(String, unique=True)
+
+    articles = relationship("Article", cascade="all,delete-orphan", backref="authors")
+    comments = relationship("Comment", cascade="all,delete-orphan", backref="authors")
+    favorites = relationship("Favorite", cascade="all,delete-orphan", backref="users")
+
+    def __repr__(self) -> str:
+        return f"User(username: {self.username}, email: {self.email})"
+
+
+class Follow(Base):
+    __tablename__ = "followers"
+
+    id = Column(Integer, primary_key=True)
+    user = Column(String(80), ForeignKey("users.username", ondelete="CASCADE"))
+    author = Column(String(80), ForeignKey("users.username", ondelete="CASCADE"))
+
+    followers = relationship(
+        "User",
+        foreign_keys=[user],
+        backref=backref("follower_user", cascade="all,delete-orphan"),
+    )
+    followings = relationship(
+        "User",
+        foreign_keys=[author],
+        backref=backref("follower_user", cascade="all,delete-orphan"),
+    )
+
+    def __repr__(self) -> str:
+        return f"Follow(user: {self.user}, author: {self.author})"
