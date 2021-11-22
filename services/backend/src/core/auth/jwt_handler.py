1ebe34ed939efb2b91aaad04ecf15d640c121667
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-22 09:47:35 +0500

    reformat project-structure

--- services/backend/src/core/auth/jwt_handler.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-from datetime import datetime as dt, timedelta
-from typing import Optional
-
-from fastapi import Depends, HTTPException, Request
-from fastapi.openapi.models import OAuthFlow as OAuthFlowModel
-from fastapi.security import OAuth2
-from fastapi.security.utils import get_authorization_scheme_param
-from jose import JWTError, jwt
