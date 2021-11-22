1c74b7944bf17bd3378653510d3e9aa02d24f0db
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-22 10:30:34 +0500

    bugfix

--- services/backend/src/repositories/users.py
+++ services/backend/src/repositories/users.py
@@ -6,10 +6,24 @@ from db.base import get_session
 from db.users import User, Follow
 from starlette.status import HTTP_401_UNAUTHORIZED
 
+from models.users import NewUserRequest
+from core import auth
+
 
 class UserRepository:
-    def create_user(self):
-        pass
+    def create_user(self, db: AsyncSession, user: NewUserRequest):
+        db_user = User(
+            token=auth.encode_jwt(user.user.email, user.user.password),
+            username=user.user.username,
+            email=user.user.email,
+            password=user.user.password,
+            bio="default",
+            image="default",
+        )
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
 
     def change_user(self):
         pass
