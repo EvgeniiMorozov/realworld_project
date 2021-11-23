c2713392232663df3645bf8cd5a08e3bac18ceac
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-23 09:37:25 +0500

    add routes

--- services/backend/src/endpoints/articles.py
+++ services/backend/src/endpoints/articles.py
@@ -1,17 +1,17 @@
 from typing import Optional
 
-from fastapi import APIRouter, HTTPException, Request, status, Response
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.params import Depends
 from slugify.slugify import slugify
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
-from core import utils
-from db.base import get_session
 from core import auth
 from crud import articles as articles_crud
-from crud import users
+from crud import users as users_crud
+from db.base import get_session
 from db.users import User
-from models.articles import GetArticles
+from models.articles import GetArticles, CreateArticleResponce, CreateArticleRequest
 
 articles_router = APIRouter()
 
@@ -23,7 +23,7 @@ def get_recent_articles_from_users_you_follow(
         limit: Optional[int] = 20,
         offset: Optional[int] = 0,
         db: AsyncSession = Depends(get_session),
-        user: User = Depends(users.get_current_user_by_token),
+        user: User = Depends(users_crud.get_current_user_by_token),
 ):
     """
     Get most recent articles from users you follow.
@@ -51,8 +51,24 @@ def get_articles(
     authorization = request.headers.get("authorization")
     if authorization:
         token = auth.clear_token(authorization)
-        auth_user = users.get_current_user_by_token(db, token)
+        auth_user = users_crud.get_current_user_by_token(db, token)
         articles = articles_crud.get_articles_auth_or_not(db, tag, author, favorited, limit, offset, auth_user)
     else:
         articles = articles_crud.get_articles_auth_or_not(db, tag, author, favorited, limit, offset)
     return GetArticles(articles=articles, articlesCount=len(articles))
+
+
+@articles_router.post("/articles", response_model=CreateArticleResponce, status_code=HTTP_201_CREATED)
+def set_up_article(
+        article_data: CreateArticleRequest,
+        db: AsyncSession,
+        user: User,
+):
+    """Create an article. Auth is required."""
+    slug = slugify(article_data.article.title)
+    article = articles_crud.get_single_article_auth_or_not_auth(db, slug)
+    if article:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="An article with this title already exists.")
+
+    article = articles_crud.create_article(db, article_data, user)
+    return CreateArticleResponce(article=article)
