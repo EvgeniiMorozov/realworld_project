52396210ed928fa97d3d069915d1ecfa658f496e
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-23 20:35:04 +0500

    added route for method create_follow

--- services/backend/src/endpoints/users.py
+++ services/backend/src/endpoints/users.py
@@ -75,3 +75,25 @@ async def get_profile(request: Request, username: str, db: AsyncSession = Depend
         curr_user = await users_crud.get_current_user_by_token(db, token)
         profile_user = await add_following(db, profile_user, curr_user)
     return ProfileUserResponce(profile=profile_user)
+
+
+@users_router.post("/profiles/{username}/follow", response_model=ProfileUserResponce, tags=["User and Authentication"])
+async def create_follow(
+    username: str,
+    db: AsyncSession = Depends(get_session),
+    follower: User = Depends(users_crud.get_current_user_by_token),
+) -> ProfileUserResponce:
+    """Follow a user by username."""
+    following = await users_crud.get_user_by_username(db, username)
+    if not following:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"User '{username}' not found")
+    if follower == following:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You cannot subscribe to yourself")
+
+    subscribe = users_crud.check_subscribe(db, follower.username, following.username)
+    if subscribe:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You are already a subscribed user")
+
+    users_crud.create_subscribe(db, user_username=follower.username, author_username=following.username)
+    following.following = True
+    return ProfileUserResponce(profile=following)
