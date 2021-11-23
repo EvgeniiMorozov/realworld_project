dbb61039d813ccd117b5c3749deb683aeff0330c
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-23 20:09:49 +0500

    add route for method update_user

--- services/backend/src/endpoints/users.py
+++ services/backend/src/endpoints/users.py
@@ -1,14 +1,20 @@
 from fastapi import HTTPException, APIRouter, Request
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN, \
-    HTTP_404_NOT_FOUND, HTTP_401_UNAUTHORIZED
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND,
+    HTTP_401_UNAUTHORIZED,
+)
 
 from core.auth import encode_jwt
 from db.base import get_session
 from crud import users as users_crud
 from db.users import User
-from models.users import UserResponce, LoginUserRequest, NewUserRequest
+from models.users import UserResponce, LoginUserRequest, NewUserRequest, UpdateUserRequest
 
 users_router = APIRouter()
 
@@ -40,3 +46,16 @@ async def current_user(user: User = Depends(users_crud.get_current_user_by_token
     if not user:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Not authorized")
     return UserResponce(user=user)
+
+
+@users_router.put("/user", response_model=UpdateUserRequest, tags=["User and Authentication"])
+async def update_user(
+    data: UpdateUserRequest,
+    db: AsyncSession = Depends(get_session),
+    user: User = Depends(users_crud.get_current_user_by_token),
+) -> UpdateUserRequest:
+    """Update user information for current user."""
+    if user:
+        new_user = await users_crud.change_user(db, user, data)
+        return UpdateUserRequest(user=new_user)
+    raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Not authorized")
