c0a40b216de0b05e471316900c6c2bf0bff35ea5
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-23 19:48:21 +0500

    add route for method authentication

---
+++ services/backend/src/endpoints/users.py
@@ -0,0 +1,26 @@
+from fastapi import HTTPException, APIRouter, Request
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN, \
+    HTTP_404_NOT_FOUND, HTTP_401_UNAUTHORIZED
+
+from core.auth import encode_jwt
+from db.base import get_session
+from crud import users as users_crud
+from db.users import User
+from models.users import UserResponce, LoginUserRequest
+
+users_router = APIRouter()
+
+
+@users_router.post("/users/login", response_model=UserResponce, tags=["User and Authentication"])
+async def authentication(user_login: LoginUserRequest, db: AsyncSession = Depends(get_session)) -> UserResponce:
+    """Login for existing user."""
+    token = encode_jwt(user_login.user.email, user_login.user.password)
+    user = await users_crud.get_user_by_token(db, token)
+    if user:
+        return UserResponce(user=user)
+    raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Not authorized")
+
+
+
