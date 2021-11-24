1b2223a28fb42b9f8d7e436f79e25c44a3e745f0
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 10:54:40 +0500

    add db models

--- services/backend/db/models.py
+++ services/backend/db/models.py
@@ -1,9 +1,6 @@
-from datetime import datetime as dt
-
-from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, ForeignKey, Text, func
 
 from db.base import metadata
-
 
 users = Table(
     "users",
@@ -14,4 +11,57 @@ users = Table(
     Column("bio", String, index=True),
     Column("image", String, nullable=True),
     Column("hashed_password", String, nullable=False),
+)
+
+followers_assoc = Table(
+    "followers_assoc",
+    metadata,
+    Column("follower", Integer, ForeignKey("users.id"), primary_key=True, index=True),
+    Column("followed_by", Integer, ForeignKey("users.id"), primary_key=True, index=True),
+)
+
+
+tags = Table(
+    "tags",
+    metadata,
+    Column("tag", String, primary_key=True, index=True),
+)
+
+
+articles = Table(
+    "articles",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("slug", String, unique=True),
+    Column("title", String),
+    Column("description", String),
+    Column("body", Text),
+    Column("author_id", Integer, ForeignKey("users.id")),
+    Column("created_at", TIMESTAMP(timezone=True), nullable=False, server_default=func.now()),
+    Column("updated_at", TIMESTAMP(timezone=True), nullable=True, server_onupdate=func.now()),
+)
+
+tags_assoc = Table(
+    "tag_assoc",
+    metadata,
+    Column("article_id", Integer, primary_key=True, index=True),
+    Column("tag", ForeignKey("tags.tag"), primary_key=True),
+)
+
+favoriter_assoc = Table(
+    "favoriter_assoc",
+    metadata,
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("article_id", Integer, ForeignKey("articles.id"), primary_key=True),
+)
+
+comments = Table(
+    "comments",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("body", String),
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("article_id", Integer, ForeignKey("articles.id")),
+    Column("created_at", TIMESTAMP(timezone=True), nullable=False, server_default=func.now()),
+    Column("updated_at", TIMESTAMP(timezone=True), nullable=True, server_onupdate=func.now()),
 )
