a50a855d28172b0aded7e8915b093d1f7cbe9f56
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 17:04:54 +0500

    added feed_articles route

--- services/backend/endpoints/articles.py
+++ services/backend/endpoints/articles.py
@@ -34,7 +34,7 @@ def gen_article_in_response(
 @router.post(
     "",
     name="Create an article",
-    description="Create an article. Auth is required",
+    description="Create an article. Auth is required.",
     response_model=models.ArticleInResponse,
 )
 async def create_article(
@@ -55,3 +55,37 @@ async def create_article(
         profile=profile,
         tags=tags,
     )
+
+
+@router.get(
+    "/feed",
+    name="Get articles from users you follow",
+    description="Get most recent articles from users you follow. Use query parameters to limit. Auth is required.",
+    response_model=models.MultipleArticlesInResponse,
+)
+async def feed_articles(
+    current_user: models.UserDB = Depends(utils.get_current_user(required=True)),
+    limit: int = 20,
+    offset: int = 0,
+) -> models.MultipleArticlesInResponse:
+    article_dbs = await crud_article.feed(limit=limit, offset=offset, follow_by=current_user.id)
+    articles = []
+    for article_db in article_dbs:
+        profile = await crud_profile.get_profile_by_user_id(article_db.author_id, requested_user=current_user)
+        tags = await crud_article.get_article_tags(article_db.id)
+        favorited = await crud_article.is_article_favorited_by_user(article_db.id, current_user.id)
+        favorites_count = await crud_article.count_article_favorites(article_db.id)
+        article_for_response = models.ArticleForResponse(
+            slug=article_db.slug,
+            title=article_db.title,
+            description=article_db.description,
+            body=article_db.body,
+            createdAt=article_db.created_at,
+            updatedAt=article_db.updated_at,
+            author=profile,
+            tagList=tags,
+            favorited=favorited,
+            favoritesCount=favorites_count,
+        )
+        articles.append(article_for_response)
+    return models.MultipleArticlesInResponse(articles=articles, articlesCount=len(articles))
