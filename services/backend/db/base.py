fb6e237b537e6662db3a252e515474055af8009b
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 10:35:35 +0500

    make db settings

--- services/backend/db/base.py
+++ services/backend/db/base.py
@@ -1,9 +1,8 @@
-from core.config import DATABASE_URL
 from databases import Database
 from sqlalchemy import MetaData, create_engine
-from sqlalchemy.ext.asyncio import create_async_engine
 
-database = Database(DATABASE_URL)
+from core.config import settings
+
+database = Database(settings.SQLALCHEMY_DATABASE_URI)
 metadata = MetaData()
-engine = create_engine(DATABASE_URL, echo=True)
-async_engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
