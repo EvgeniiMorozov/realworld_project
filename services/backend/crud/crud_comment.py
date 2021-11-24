59b65f758cb7272c97d95af215d3c6b6f8770e01
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 14:01:46 +0500

    added method delete for comment

--- services/backend/crud/crud_comment.py
+++ services/backend/crud/crud_comment.py
@@ -24,3 +24,8 @@ async def get_comments_from_an_article(article_id: int) -> list[models.CommentDB
     query = db.comments.select().where(article_id == db.comments.c.article_id)
     comment_rows = await database.fetch_all(query=query)
     return [models.CommentDB(**row) for row in comment_rows]
+
+
+async def delete(comment_id: int) -> None:
+    query = db.comments.delete().where(comment_id == db.comments.c.id)
+    await database.execute(query=query)
