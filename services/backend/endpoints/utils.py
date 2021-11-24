c879972fd0f0dac05d16d5c2b634308bec787ddf
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 14:19:27 +0500

    added authorization_header_token_optional method

--- services/backend/endpoints/utils.py
+++ services/backend/endpoints/utils.py
@@ -21,3 +21,17 @@ def authorization_header_token_required(api_key: str = Depends(APIKeyHeader(name
     if token_prefix != JWT_TOKEN_PREFIX:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="unsupported authorization type")
     return token
+
+
+def authorization_header_token_optional(
+    api_key: str = Depends(APIKeyHeader(name="Authorization", auto_error=False))
+) -> Optional[str]:
+    if api_key is None:
+        return None
+    try:
+        token_prefix, token = api_key.split(" ")
+    except ValueError:
+        return None
+    if token_prefix != JWT_TOKEN_PREFIX:
+        return None
+    return token
