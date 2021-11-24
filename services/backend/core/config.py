dae6fa92f734827513efa88ed062b50e8c25dfe9
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 10:13:52 +0500

    make settings

--- services/backend/core/config.py
+++ services/backend/core/config.py
@@ -1,6 +1,33 @@
-from starlette.config import Config
+import secrets
+from typing import Any, Optional
+
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
-config = Config(".env")
+class Settings(BaseSettings):
+    TESTING: bool = False
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    POSTGRES_SERVER: str = "127.0.0.1"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "realworld"
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
-DATABASE_URL = config("RW_DATABASE_URL", cast=str, default="")
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+
+        db_prefix = "test_" if values.get("TESTING") else ""
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{db_prefix}{values.get('POSTGRES_DB') or ''}"
+        )
+
+
+settings = Settings()
