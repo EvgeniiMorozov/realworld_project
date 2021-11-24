4dce270be2dfab67c2b1779cb569ded675fe49f3
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-24 20:53:37 +0500

    added delete_comment_for_article route

--- services/backend/endpoints/comments.py
+++ services/backend/endpoints/comments.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Body, Depends, HTTPException
-from starlette.status import HTTP_400_BAD_REQUEST
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
 
 import models
-from endpoints.utils import get_current_user
 from crud import crud_article, crud_comment, crud_profile
+from endpoints.utils import get_current_user
 
 SLUG_NOT_FOUND = "article with this slug not found"
 
@@ -68,3 +68,31 @@ async def get_comments_from_an_article(
             )
         )
     return models.MultipleCommentsInResponse(comments=comments)
+
+
+@router.delete(
+    "/{comment_id}",
+    name="Delete a comment for an article",
+    description="Delete a comment for an article. Auth is required.",
+)
+async def delete_comment_for_article(
+    slug: str,
+    comment_id: int,
+    current_user: models.UserDB = Depends(get_current_user(required=True)),
+) -> None:
+    article_db = await crud_article.get_article_by_slug(slug=slug)
+    if article_db is None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=SLUG_NOT_FOUND)
+
+    comment_db = await crud_comment.get(comment_id)
+    if comment_db is None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="comment with this id not found")
+
+    if comment_db.article_id != article_db.id:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="this comment is not belong to this article")
+
+    if comment_db.author_id != current_user.id:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="You can not delete a comment ids not belong to yourself"
+        )
+    await crud_comment.delete(comment_id)
