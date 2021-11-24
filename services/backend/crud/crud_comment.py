3865b1168f8e71bde54daf4bb9c92c4035965d32
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 13:59:56 +0500

    added method get_comments_from_an_article for comment

--- services/backend/crud/crud_comment.py
+++ services/backend/crud/crud_comment.py
@@ -20,3 +20,7 @@ async def get(comment_id: int) -> Optional[models.CommentDB]:
     return models.CommentDB(**comment_row) if comment_row else None
 
 
+async def get_comments_from_an_article(article_id: int) -> list[models.CommentDB]:
+    query = db.comments.select().where(article_id == db.comments.c.article_id)
+    comment_rows = await database.fetch_all(query=query)
+    return [models.CommentDB(**row) for row in comment_rows]
