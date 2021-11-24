3e9d56a802fbfa44fa8043b683fc7ed152034dbc
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 15:42:09 +0500

    added unfollow_user route

--- services/backend/endpoints/profiles.py
+++ services/backend/endpoints/profiles.py
@@ -17,9 +17,7 @@ async def get_profile_response(username: str, requested_user: models.UserDB) -> 
     return models.ProfileResponse(profile=profile)
 
 
-async def get_follow_user(
-        username: str, requested_user: models.UserDB
-) -> models.UserDB:
+async def get_follow_user(username: str, requested_user: models.UserDB) -> models.UserDB:
     follower_user = await crud_user.get_user_by_username(username=username)
     if follower_user is None:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="user with this username is not existed")
@@ -36,8 +34,8 @@ async def get_follow_user(
     response_model=models.ProfileResponse,
 )
 async def get_profile(
-        username: str,
-        requested_user: models.UserDB = Depends(utils.get_current_user(required=False)),
+    username: str,
+    requested_user: models.UserDB = Depends(utils.get_current_user(required=False)),
 ) -> models.ProfileResponse:
     return await get_profile_response(requested_user=requested_user, username=username)
 
@@ -49,11 +47,36 @@ async def get_profile(
     response_model=models.ProfileResponse,
 )
 async def follow_user(
-        username: str,
-        requested_user: models.UserDB = Depends(utils.get_current_user),
+    username: str,
+    requested_user: models.UserDB = Depends(utils.get_current_user),
 ) -> models.ProfileResponse:
     follower_user = await get_follow_user(requested_user=requested_user, username=username)
     if await crud_profile.is_following(follower_user, requested_user):
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="you follow this user already")
     await crud_profile.follow(follower_user, requested_user)
     return await get_profile_response(requested_user=requested_user, username=username)
+
+
+@router.delete(
+    "/{username}/follow",
+    name="Unfollow a user",
+    description="Unfollow a user by username",
+    response_model=models.ProfileResponse,
+)
+async def unfollow_user(
+    username: str,
+    requested_user: models.UserDB = Depends(utils.get_current_user),
+) -> models.ProfileResponse:
+    follower_user = await crud_user.get_user_by_username(username=username)
+    if follower_user is None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="user with this username is not existed")
+
+    if follower_user.id == requested_user.id:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="cannot unfollow yourself")
+
+    if not await crud_profile.is_following(follower_user, requested_user):
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="you don`t follow this user already")
+
+    await crud_profile.unfollow(follower_user, requested_user)
+    profile = await crud_profile.get_profile_by_username(username, requested_user)
+    return models.ProfileResponse(profile=profile)
