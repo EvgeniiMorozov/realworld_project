ce46982ccad52b469cfa7835a15d4111deca9bdb
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 12:56:23 +0500

    added method create for article

--- services/backend/crud/crud_article.py
+++ services/backend/crud/crud_article.py
@@ -34,3 +34,21 @@ async def get_article_tags(article_id: int) -> list[str]:
     )
     tags = await database.fetch_all(query=query)
     return [tag.get("tag") for tag in tags]
+
+
+async def create(payload: models.ArticleInCreate, author_id: int) -> int:
+    slug = slugify(payload.title)
+    query = db.articles.insert().values(
+        title=payload.title,
+        description=payload.description,
+        body=payload.body,
+        slug=slug,
+        author_id=author_id,
+    )
+    article_id = await database.execute(query=query)
+    if payload.tagList:
+        for tag in payload.tagList:
+            if not await crud_tag.is_existing_tag(tag):
+                await crud_tag.create(tag)
+        await add_article_tags(article_id, payload.tagList)
+    return article_id
