aab6c2c5340193d2a9aa8efa4609df9cc7362cf1
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-24 13:57:21 +0500

    added method get for comment

--- services/backend/crud/crud_comment.py
+++ services/backend/crud/crud_comment.py
@@ -12,3 +12,11 @@ async def create(payload: models.CommentInCreate, article_id: int, author_id: in
         article_id=article_id,
     )
     return await database.execute(query=query)
+
+
+async def get(comment_id: int) -> Optional[models.CommentDB]:
+    query = db.comments.select().where(comment_id == db.comments.c.id)
+    comment_row = await database.fetch_one(query=query)
+    return models.CommentDB(**comment_row) if comment_row else None
+
+
