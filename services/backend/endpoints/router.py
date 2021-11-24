0c715df64728a5d0073f0866e96ef72a95236886
EvgeniiMorozov <emorozov1101@gmail.com>
2021-11-24 21:21:08 +0500

    create router

---
+++ services/backend/endpoints/router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from endpoints import articles, authentication, comments, tags, profiles, users
+
+api_router = APIRouter()
+
+api_router.include_router(authentication.router, tags=["User and Authentication"], prefix="/users")
+api_router.include_router(users.router, tags=["User and Authentication"], prefix="/user")
+api_router.include_router(profiles.router, tags=["Profiles"], prefix="/profiles")
+api_router.include_router(tags.router, tags=["Tags"], prefix="/tags")
+api_router.include_router(articles.router, tags=["Articles"], prefix="/articles")
+api_router.include_router(comments.router, tags=["Comments"], prefix="/articles/{slug}/comments")
