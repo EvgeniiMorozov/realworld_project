8ce8dd78232d8c167cb1bdc389fd42b5cc22f59d
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-11-26 10:09:24 +0500

    added make_corrections_in_the_alembic_ini func

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,9 +1,16 @@
+import configparser
+import pathlib
+
 from fastapi import FastAPI
 from uvicorn import run
 from loguru import logger
 
+from core.config import settings
 from db.base import database
 from endpoints.router import api_router
+
+CURR_PATH = pathlib.Path()
+TO_ALEMBIC_INI = CURR_PATH.parent
 
 
 app = FastAPI()
@@ -26,6 +33,12 @@ async def startup():
 async def shutdown():
     logger.info("Disconnect to database")
     await database.disconnect()
+
+
+def make_corrections_in_the_alembic_ini():
+    if not pathlib.Path(TO_ALEMBIC_INI / "alembic.ini").exists():
+        pass
+    config = configparser.ConfigParser()
 
 
 if __name__ == "__main__":
