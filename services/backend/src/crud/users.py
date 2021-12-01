aa286c34592c911cbde9dd486774b7cf5e62b0fe
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-01 19:30:37 +0500

    refactor

--- services/backend/src/crud/users.py
+++ services/backend/src/crud/users.py
@@ -13,7 +13,7 @@ from src.db.users import User
 from src.models.users import NewUserRequest, UpdateUserRequest, UserResponce, User as UserDB
 
 
-async def create_user(user: NewUserRequest, session: AsyncSession = Depends(get_session)) -> UserDB:
+async def create_user(user: NewUserRequest, session: AsyncSession = Depends(get_session),) -> UserDB:
     """Create and return a created User model."""
     db_user = User(
         token=auth.encode_jwt(user.user.email, user.user.password),
@@ -29,7 +29,9 @@ async def create_user(user: NewUserRequest, session: AsyncSession = Depends(get_
     return db_user
 
 
-async def change_user(user: UserResponce, data: UpdateUserRequest, session: AsyncSession = Depends(get_session)) -> User:
+async def change_user(
+    user: UserResponce, data: UpdateUserRequest, session: AsyncSession = Depends(get_session),
+) -> User:
     """Update and return User model."""
     upd_user = update(User).where(User.token == user.user.token).values(**data.user.dict(exclude_unset=True))
     # upd_user = update(User).where(User.token == user.token).values(**data.user.dict(exclude_unset=True))
@@ -38,7 +40,10 @@ async def change_user(user: UserResponce, data: UpdateUserRequest, session: Asyn
     return await session.query(User).filter(User.token == user.user.token).first()
 
 
-async def get_current_user_by_token(session: AsyncSession = Depends(get_session), token: str = Depends(auth.check_token)) -> UserDB:
+async def get_current_user_by_token(
+    session: AsyncSession = Depends(get_session),
+    token: str = Depends(auth.check_token),
+) -> UserDB:
     """Getting a User model from a token and checking that the user exists."""
     user = await get_user_by_token(session, token)
     if not user:
@@ -51,9 +56,9 @@ async def get_user_by_token(session: AsyncSession, token: str) -> User:
     return await session.query(User).filter(User.token == token).first()
 
 
-def get_user_by_username(db: AsyncSession, username: str) -> User:
+async def get_user_by_username(session: AsyncSession, username: str) -> User:
     """Get User model by username."""
-    return db.query(User).filter(User.username == username).first()
+    return await session.query(User).filter(User.username == username).first()
 
 
 def get_user_by_email(db: AsyncSession, email: EmailStr) -> User:
