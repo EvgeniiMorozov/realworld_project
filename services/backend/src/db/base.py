acca07f8fcb097ce55fc90a633e466bad57a19b6
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-12-07 15:47:43 +0500

    update

--- services/backend/src/db/base.py
+++ services/backend/src/db/base.py
@@ -1,6 +1,9 @@
+from typing import Any
+
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
 # from sqlalchemy.orm import as_declarative
 
@@ -8,6 +11,15 @@ from src.core.config import DATABASE_URL
 
 
 async_engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+
+@as_declarative()
+class Base(object):
+    id = Column(Integer, primary_key=True, index=True)
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
 
 
 async def init_db():
