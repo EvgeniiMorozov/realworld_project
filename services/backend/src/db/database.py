ac06cef185a3fd4dee3fce11518055b5469fe3b3
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-08 21:17:43 +0500

    updated

--- services/backend/src/db/database.py
+++ services/backend/src/db/database.py
@@ -3,13 +3,12 @@ from tortoise.models import Model
 
 
 class TimestampMixin:
-    created_at = DatetimeField(auto_now_add=True)
-    updated_at = DatetimeField(auto_now=True)
+    created_at = DatetimeField(auto_now_add=True, description="Дата создания")
+    updated_at = DatetimeField(auto_now=True, description="Дата обновления")
 
 
 class BaseModel(Model):
-    id = IntField(pk=True)
+    id = IntField(pk=True, index=True, description="ID")
 
     class Meta:
         abstract = True
-        # table = cls.__name__.lower()
