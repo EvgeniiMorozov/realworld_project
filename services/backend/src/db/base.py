13b898986e6ae446e0f74addc3a3a3bf19cf42f3
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-11 21:54:07 +0500

    refactor

--- services/backend/src/db/base.py
+++ services/backend/src/db/base.py
@@ -1,20 +1,10 @@
-from sqlalchemy import Column
-from sqlalchemy import func
-from sqlalchemy import Integer
-from sqlalchemy import TIMESTAMP
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import TIMESTAMP, Column, Integer, func
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import declarative_mixin
-from sqlalchemy.orm import sessionmaker
-from src.core.config import settings
-
-async_engine = create_async_engine(settings.DATABASE_URI, echo=True, future=True)
 
 
 @as_declarative()
 class Base(object):
-
     id = Column(Integer, primary_key=True, index=True)
 
 
@@ -22,18 +12,3 @@ class Base(object):
 class TimestampMixin:
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
-
-
-async def init_db():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def get_session() -> AsyncSession:
-    async_session = sessionmaker(
-        async_engine,
-        class_=AsyncSession,
-        expire_on_commit=False,
-    )
-    async with async_session() as session:
-        yield session
