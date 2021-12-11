58695755515e9c882a658b926574fe08646f2555
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-11 22:01:48 +0500

    added connection to db

---
+++ services/backend/src/db/connection.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from src.core.config import settings
+
+engine = create_async_engine(settings.async_database_url, echo=True)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
