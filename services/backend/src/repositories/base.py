d1163da8c668004e8d984d799982dd34f19455b7
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-12 21:13:22 +0500

    added base_repository

---
+++ services/backend/src/repositories/base.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class BaseRepository:
+    def __init__(self, db_session: AsyncSession, *args, **kwargs) -> None:
+        self._db_session: AsyncSession = db_session
