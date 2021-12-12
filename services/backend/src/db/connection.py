d7e95ed695ef18cdf061654dffdf5cf12a932d69
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-12 09:36:38 +0500

    added dependency

--- services/backend/src/db/connection.py
+++ services/backend/src/db/connection.py
@@ -1,6 +1,13 @@
+from typing import AsyncIterator
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from src.core.config import settings
 
 engine = create_async_engine(settings.async_database_url, echo=True)
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db() -> AsyncIterator[AsyncSession]:
+    async with async_session() as session:
+        yield session
+        await session.commit()
