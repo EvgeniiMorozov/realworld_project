129a0e45b58b42c72451f1ea01205f287ab85aba
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-12 19:51:38 +0500

    added comment`s schemas

---
+++ services/backend/src/schemas/comment.py
@@ -0,0 +1,35 @@
+import datetime
+
+from pydantic import BaseModel
+from src import schemas
+
+
+class CommentBase(BaseModel):
+    body: str
+
+
+class CommentDB(CommentBase):
+    id: int
+    author_id: int
+    article_id: int
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+
+class CommentInCreate(CommentBase):
+    pass
+
+
+class CommentForResponse(CommentBase):
+    id: int
+    createdAt: datetime.datetime
+    updatedAt: datetime.datetime
+    author: schemas.Profile
+
+
+class CommentInResponse(BaseModel):
+    comment: CommentForResponse
+
+
+class MultipleCommentsInResponse(BaseModel):
+    comments: list[CommentForResponse]
