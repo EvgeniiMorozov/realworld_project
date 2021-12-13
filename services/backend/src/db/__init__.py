90e05c83e5c29cd191a2919f6453dcf409dd4967
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-12-13 12:27:08 +0500

    update init

--- services/backend/src/db/__init__.py
+++ services/backend/src/db/__init__.py
@@ -1,1 +1,10 @@
-from connection import async_session, engine, get_db
+from .connection import async_session, engine, get_db
+from .models import *
+
+
+users = User()
+articles = Article()
+comments = Comment()
+tags = Tag()
+followers = Follow()
+favorites = Favorite()
