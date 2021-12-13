b2bd4fbe3b5fc67bb6b5de39a318a7d2bc619a5c
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2021-12-13 11:49:05 +0500

    added security dependecys

---
+++ services/backend/src/core/security.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+from typing import Any, Union
+
+from fastapi import HTTPException
+from jose import jwt
+from passlib.context import CryptContext
+from pydantic import SecretStr, ValidationError
+from starlette import status
+
+from src.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: timedelta = None) -> str:
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expire, "sub": str(subject)}
+
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_user_id_from_token(token: str) -> str:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=ALGORITHM)
+        return payload.get("sub", None)
+
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
+
+
+def verify_password(plain_password: SecretStr, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password.get_secret_value(), hashed_password)
+
+
+def get_password_hash(password: SecretStr) -> str:
+    return pwd_context.hash(password.get_secret_value())
