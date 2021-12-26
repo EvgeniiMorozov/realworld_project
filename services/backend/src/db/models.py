d5bd957fdb1d2e75674ba75baa1895d97daff2bc
EvgeniiMorozov <emorozov1101@gmail.com>
2021-12-26 19:42:17 +0500

    update models

--- services/backend/src/db/models.py
+++ services/backend/src/db/models.py
@@ -2,35 +2,51 @@ from tortoise import fields
 from database import BaseModel, TimestampMixin
 
 
-class Users(TimestampMixin, BaseModel):
+class User(TimestampMixin, BaseModel):
     username = fields.CharField(max_length=80, unique=True, null=False)
     email = fields.CharField(max_length=100, unique=True, null=False)
     password_hash = fields.CharField(max_length=128, null=True)
     bio = fields.CharField(max_length=300, null=True)
     image = fields.CharField(max_length=120, null=True)
 
-
-class Follow(BaseModel):
-    user = fields.ForeignKeyField("models.Users", to_field="username")
-    author = fields.ForeignKeyField("models.Users", to_field="username")
+    class Meta:
+        table = "users"
 
 
-class Articles(TimestampMixin, BaseModel):
+class Profile(User):
+    follower = fields.ManyToManyField("modeles.Profile")
+
+
+# class Follow(BaseModel):
+#     user = fields.ForeignKeyField("models.Users", to_field="username")
+#     author = fields.ForeignKeyField("models.Users", to_field="username")
+
+
+class Article(TimestampMixin, BaseModel):
     slug = fields.CharField(max_length=100, unique=True, index=True)
     title = fields.CharField(max_length=100)
     description = fields.CharField(max_length=300)
     body = fields.TextField()
     author = fields.ForeignKeyField("models.Users", related_name="article")
 
+    class Meta:
+        table = "articles"
+
     def __str__(self) -> str:
         return f"{self.title}, {self.author} on {self.created_at}"
 
 
-class Tags(BaseModel):
+class Tag(BaseModel):
     name = fields.CharField(max_length=50, unique=True)
 
+    class Meta:
+        table = "tags"
 
-class Comments(TimestampMixin, BaseModel):
+
+class Comment(TimestampMixin, BaseModel):
     body = fields.TextField()
     author = fields.ForeignKeyField("models.Users", related_name="comment")
     article = fields.ForeignKeyField("models.Articles", related_name="comment", to_field="slug")
+
+    class Meta:
+        table = "comments"
