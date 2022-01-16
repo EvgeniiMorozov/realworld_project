e9258ecc5ddf0b0b9c0c484ae6bcac28b13f3750
EvgeniiMorozov <emorozov1101@gmail.com>
2022-01-16 18:50:58 +0500

    add types-hints

--- services/backend/src/db/base.py
+++ services/backend/src/db/base.py
@@ -1,7 +1,8 @@
 from sqlalchemy import TIMESTAMP, Column, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_mixin, Mapped
-from sqlalchemy.orm.decl_api import DeclarativeMeta
+from sqlalchemy.orm.decl_api import DeclarativeMeta, declared_attr
+from sqlalchemy.sql.sqltypes import DateTime
 
 
 # class Base(metaclass=DeclarativeMeta):
@@ -12,5 +13,12 @@ Base = declarative_base()
 
 @declarative_mixin
 class TimestampMixin:
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
-    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    # created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    # updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    @declared_attr
+    def created_at(cls) -> Column[DateTime]:
+        return Column(TIMESTAMP(timezone=True),nullable=False,  server_default=func.now())
+
+    @declared_attr
+    def updated_at(cls) -> Column[DateTime]:
+        return Column(TIMESTAMP(timezone=True),nullable=False,  server_default=func.now())
