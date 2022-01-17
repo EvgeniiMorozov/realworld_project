ce777e04090ac0266aba41a08b660f293025cb72
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-01-17 11:51:05 +0500

    add url to db

--- app/db/base.py
+++ app/db/base.py
@@ -3,7 +3,7 @@ from sqlalchemy import MetaData, create_engine
 
 from core.config import settings
 
-print(f"******* {settings.SQLALCHEMY_DATABASE_URI}")
-database = Database(settings.SQLALCHEMY_DATABASE_URI)
+print(f"******* {settings.async_database_url}")
+database = Database(settings.async_database_url)
 metadata = MetaData()
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
+engine = create_engine(settings.async_database_url, echo=True)
