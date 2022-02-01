6e7a5d484a229c462a648ca885581fe08a278653
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-02-01 15:55:33 +0500

    remove unused imports

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-import configparser
 import pathlib
 
 from fastapi import FastAPI
 from uvicorn import run
-# from loguru import logger
 
-from core.config import settings
 from db import database
 from endpoints.routers import api_router
+
+# from loguru import logger
+
 
 CURR_PATH = pathlib.Path()
 TO_ALEMBIC_INI = CURR_PATH.parent
@@ -33,12 +33,6 @@ async def startup():
 async def shutdown():
     # logger.info("Disconnect to database")
     await database.disconnect()
-
-
-def make_corrections_in_the_alembic_ini():
-    if not pathlib.Path(TO_ALEMBIC_INI / "alembic.ini").exists():
-        pass
-    config = configparser.ConfigParser()
 
 
 if __name__ == "__main__":
