ae89ecc737cd192b51718cd03aa6a089988b88dd
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-02-03 17:05:34 +0500

    adding CRUD methods for User model

--- services/backend/src/crud/user.py
+++ services/backend/src/crud/user.py
@@ -14,7 +14,7 @@ async def get_current_user_by_token(
     db: AsyncSession = Depends(get_db),
     token: str = Depends(auth.check_token),
 ) -> db_User:
-    """Getting a User model from a token and checking that the user exists."""
+    """Getting a User model from a token and checking that the user exists"""
     user = await get_user_by_token(db, token)
     if not user:
         raise HTTPException(
@@ -27,3 +27,27 @@ async def get_user_by_token(db: AsyncSession, token: str) -> db_User:
     """Get User model by token"""
     user = await db.execute(select(db_User).filter(db_User.token == token))
     return user.scalars().first()
+
+
+async def get_user_by_username(db: AsyncSession, username: str) -> db_User:
+    """Get User model by username"""
+    user = await db.execute(select(db_User).filter(db_User.username == username))
+    return user.scalars().first()
+
+
+async def get_user_by_email(db: AsyncSession, email: str) -> db_User:
+    """Get User model by email"""
+    user = await db.execute(select(db_User).filter(db_User.email == email))
+    return user.scalars().first()
+
+
+async def create_user(db: AsyncSession, new_user: user.UserCreate) -> db_User:
+    """Create and return a created User model"""
+    db_user = db_User(
+        token=auth.encode_jwt(new_user.email, new_user.password),
+        username=new_user.user.username,
+        email=new_user.email,
+        )
+    db.add(db_user)
+    await db.commit()
+    return db_user
