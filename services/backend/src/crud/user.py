38f49cd5e33e690457d3036da2f990b6a5f73022
EvgeniiMorozov <emorozov1101@gmail.com>
2022-02-03 20:12:54 +0500

    update

--- services/backend/src/crud/user.py
+++ services/backend/src/crud/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import HTTPException
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,48 +8,59 @@ from starlette.status import HTTP_401_UNAUTHORIZED
 
 from src.db.connection import get_db
 from src.db.models import Follow as db_Follow, User as db_User
-from src.core import auth
-from src.schemas import user, profile
+from src.core import auth, security
+from src.schemas import user as user_schema, profile as profile_schema
 
 
-async def get_current_user_by_token(
-    db: AsyncSession = Depends(get_db),
-    token: str = Depends(auth.check_token),
-) -> db_User:
-    """Getting a User model from a token and checking that the user exists"""
-    user = await get_user_by_token(db, token)
-    if not user:
-        raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED, detail="User not authorized"
-        )
-    return user
+# async def get_current_user_by_token(db: AsyncSession, token: str) -> db_User:
+#     """Getting a User model from a token and checking that the user exists"""
+#     query = await get_user_by_token(db, token)
+#     if not user:
+#         raise HTTPException(
+#             status_code=HTTP_401_UNAUTHORIZED, detail="User not authorized"
+#         )
+#     return user
+#
+#
+# async def get_user_by_token(db: AsyncSession, token: str) -> db_User:
+#     """Get User model by token"""
+#     query = await db.execute(select(db_User).filter(db_User.token == token))
+#     return query.scalars().first()
+async def get_user_by_id(
+    db: AsyncSession, user_id: int
+) -> Optional[user_schema.UserDB]:
+    """Get user by id"""
+    user_db = await db.execute(select(db_User).filter(db_User.id == user_id))
+    return user_db.scalars().first()
 
 
-async def get_user_by_token(db: AsyncSession, token: str) -> db_User:
-    """Get User model by token"""
-    user = await db.execute(select(db_User).filter(db_User.token == token))
-    return user.scalars().first()
-
-
-async def get_user_by_username(db: AsyncSession, username: str) -> db_User:
-    """Get User model by username"""
+async def get_user_by_username(
+    db: AsyncSession, username: str
+) -> Optional[user_schema.UserDB]:
+    """Get user by username"""
     user = await db.execute(select(db_User).filter(db_User.username == username))
     return user.scalars().first()
 
 
-async def get_user_by_email(db: AsyncSession, email: str) -> db_User:
-    """Get User model by email"""
+async def get_user_by_email(
+    db: AsyncSession, email: str
+) -> Optional[user_schema.UserDB]:
+    """Get user by email"""
     user = await db.execute(select(db_User).filter(db_User.email == email))
     return user.scalars().first()
 
 
-async def create_user(db: AsyncSession, new_user: user.UserCreate) -> db_User:
-    """Create and return a created User model"""
-    db_user = db_User(
-        token=auth.encode_jwt(new_user.email, new_user.password),
-        username=new_user.user.username,
-        email=new_user.email,
-        )
-    db.add(db_user)
+async def create_user(
+    db: AsyncSession, payload: user_schema.UserCreate
+) -> Optional[int]:
+    """Create and return a created user"""
+    new_user = db_User(
+        username=payload.username,
+        email=payload.email,
+        password_hash=security.get_password_hash(payload.password),
+        token=auth.encode_jwt(payload.email, payload.password),
+    )
+    db.add(new_user)
     await db.commit()
-    return db_user
+    return new_user.id
+
