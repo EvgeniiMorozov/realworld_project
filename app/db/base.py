920730371695d398ca74a05097253de3088a3943
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-02-04 14:56:23 +0500

    update

--- app/db/base.py
+++ app/db/base.py
@@ -1,9 +1,12 @@
-from databases import Database
-from sqlalchemy import MetaData, create_engine
-
 from core.config import settings
+from databases import Database
+from sqlalchemy import (
+    MetaData,
+    create_engine
+)
 
-print(f"******* {settings.async_database_url}")
-database = Database(settings.async_database_url)
+
+# print(f"******* {settings.async_database_url}")
+database = Database(settings.SQLALCHEMY_DATABASE_URI)
 metadata = MetaData()
-engine = create_engine(settings.async_database_url, echo=True)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
