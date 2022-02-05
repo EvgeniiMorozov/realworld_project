a4728e15c70552d3383afe85d3daeab8d426c2f5
EvgeniiMorozov <emorozov1101@gmail.com>
2022-02-05 11:19:48 +0500

    update alembic settings

--- alembic/env.py
+++ alembic/env.py
@@ -2,9 +2,13 @@ import pathlib
 import sys
 from logging.config import fileConfig
 
+from sqlalchemy import (
+    engine_from_config,
+    pool
+)
+
 from alembic import context
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+
 
 sys.path.append(str(pathlib.Path().absolute()))
 
