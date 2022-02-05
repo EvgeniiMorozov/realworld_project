fde403d56671c6da7353ffc765e877dc44f79427
EvgeniiMorozov <emorozov1101@gmail.com>
2022-02-05 21:18:26 +0500

    reformatting

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, ForeignKey, Text, func
-
 from db.base import metadata
+from sqlalchemy import (
+    TIMESTAMP,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Table,
+    Text,
+    func,
+)
 
 users = Table(
     "users",
@@ -16,8 +24,20 @@ users = Table(
 followers_assoc = Table(
     "followers_assoc",
     metadata,
-    Column("follower", Integer, ForeignKey("users.id"), primary_key=True, index=True),
-    Column("followed_by", Integer, ForeignKey("users.id"), primary_key=True, index=True),
+    Column(
+        "follower",
+        Integer,
+        ForeignKey("users.id"),
+        primary_key=True,
+        index=True,
+    ),
+    Column(
+        "followed_by",
+        Integer,
+        ForeignKey("users.id"),
+        primary_key=True,
+        index=True,
+    ),
 )
 
 tags = Table(
@@ -35,8 +55,18 @@ articles = Table(
     Column("description", String),
     Column("body", Text),
     Column("author_id", Integer, ForeignKey("users.id")),
-    Column("created_at", TIMESTAMP(timezone=True), nullable=False, server_default=func.now()),
-    Column("updated_at", TIMESTAMP(timezone=True), nullable=True, server_default=func.now()),
+    Column(
+        "created_at",
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    ),
+    Column(
+        "updated_at",
+        TIMESTAMP(timezone=True),
+        nullable=True,
+        server_default=func.now(),
+    ),
 )
 
 tags_assoc = Table(
@@ -60,6 +90,16 @@ comments = Table(
     Column("body", String),
     Column("user_id", Integer, ForeignKey("users.id")),
     Column("article_id", Integer, ForeignKey("articles.id")),
-    Column("created_at", TIMESTAMP(timezone=True), nullable=False, server_default=func.now()),
-    Column("updated_at", TIMESTAMP(timezone=True), nullable=True, server_onupdate=func.now()),
+    Column(
+        "created_at",
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    ),
+    Column(
+        "updated_at",
+        TIMESTAMP(timezone=True),
+        nullable=True,
+        server_onupdate=func.now(),
+    ),
 )
