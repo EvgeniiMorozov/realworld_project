83e590e7a93ad4191c0ee5f20a77e290e2f011bc
EvgeniiMorozov <emorozov1101@gmail.com>
2022-02-08 21:04:38 +0500

    upd

--- services/backend/src/repositories/user.py
+++ services/backend/src/repositories/user.py
@@ -2,12 +2,11 @@ import asyncio
 import datetime
 from typing import Optional
 
-from pydantic import SecretStr, EmailStr
-from sqlalchemy.sql.expression import select
-
 import src.db as db
 import src.schemas as schemas
 from base import BaseRepository
+from pydantic import EmailStr, SecretStr
+from sqlalchemy import select
 from src.core.security import get_password_hash
 
 
@@ -17,10 +16,9 @@ class UserRepository(BaseRepository):
             username=payload.username,
             email=payload.email,
             hashed_password=get_password_hash(payload.password),
-            )
-        self.db_session.add(new_user)
-        await self.db_session.flush()
-        return new_user
+        )
+        result = await self.db_session.execute(new_user)
+        return result.scalars().first()
 
     async def get(self, user_id: int) -> Optional[schemas.UserDB]:
         query = select(db.User).where(db.User.c.id == user_id)
@@ -28,7 +26,9 @@ class UserRepository(BaseRepository):
         await self.db_session.flush()
         return schemas.UserDB(**result.scalars().first()) if result else None
 
-    async def get_by_email(self, user_email: EmailStr) -> Optional[schemas.UserDB]:
+    async def get_by_email(
+        self, user_email: EmailStr
+    ) -> Optional[schemas.UserDB]:
         query = select(db.User).where(db.User.c.email == user_email)
         result = await self.db_session.execute(query)
         await self.db_session.flush()
