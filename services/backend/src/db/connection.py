30a8e3d78814ef2cdb1f2a0f7ad1fead711b8527
EvgeniiMorozov <emorozov1101@gmail.com>
2022-02-16 20:50:54 +0500

    Remove unused imports

--- services/backend/src/db/connection.py
+++ services/backend/src/db/connection.py
@@ -1,11 +1,6 @@
-from typing import AsyncGenerator, Tuple
-
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from sqlalchemy.orm import sessionmaker
 from src.core.config import settings
-from starlette.requests import Request
 
 async_engine = create_async_engine(
     settings.DATABASE_URI, future=True, echo=True
