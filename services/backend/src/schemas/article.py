abb0f586ebe88341a70d297053f3ca043a97b09b
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-02-16 14:55:19 +0500

    Added pydantic schemas for Article model

--- services/backend/src/schemas/article.py
+++ services/backend/src/schemas/article.py
@@ -1,95 +1,49 @@
-from datetime import datetime
+import datetime
 from typing import Optional
 
 from pydantic import BaseModel
-from src.schemas.users import ProfileUser
+
+from .profile import Profile
 
 
-class Article(BaseModel):
-    slug: str
+class ArticleBase(BaseModel):
     title: str
     description: str
     body: str
-    tagList: Optional[list[str]] = []
-    createdAt: datetime
-    updatedAt: datetime
-    favorited: Optional[bool] = False
-    favoritesCount: Optional[int] = 0
-    author: ProfileUser
-
-    class Config:
-        orm_mode = True
 
 
-class GetArticles(BaseModel):
-    articles: list[Article]
-    articlesCount: Optional[int] = 0
-
-    class Config:
-        orm_mode = True
-
-
-class CreateArticle(BaseModel):
-    title: str
-    description: str
-    body: str
-    tagList: Optional[list[str]] = None
-
-
-class CreateArticleRequest(BaseModel):
-    article: CreateArticle
-
-
-class CreateArticleResponce(BaseModel):
-    article: Article
-
-
-class GetArticle(BaseModel):
-    article: Article
-
-    class Config:
-        orm_mode = True
-
-
-class ChangeArticle(BaseModel):
-    title: Optional[str] = None
-    description: Optional[str] = None
-    body: Optional[str] = None
-
-
-class UpdateArticle(BaseModel):
-    article: ChangeArticle
-
-
-class Comment(BaseModel):
+class ArticleDB(ArticleBase):
     id: int
-    createdAt: datetime
-    updatedAt: datetime
-    body: str
-    author: ProfileUser
-
-    class Config:
-        orm_mode = True
+    slug: str
+    author_id: int
+    ctreated_at: datetime.datetime
+    updated_at: datetime.datetime
 
 
-class GetCommentsResponse(BaseModel):
-    comments: list[Comment]
+class ArticleInCreate(ArticleBase):
+    tagList: Optional[list[str]]
 
 
-class CreateCommentBody(BaseModel):
-    body: str
+class ArticleForResponse(ArticleBase):
+    slug: str
+    author: Profile
+    createdAt: datetime.datetime
+    updatedAt: datetime.datetime
+    favorited: bool
+    favoritesCount: int
 
 
-class CreateComment(BaseModel):
-    comment: CreateCommentBody
+class ArticleInResponse(BaseModel):
+    article: ArticleForResponse
 
 
-class GetCommentResponse(BaseModel):
-    comment: Comment
+class ArticleInUpdate(BaseModel):
+    title: Optional[str]
+    description: Optional[str]
+    body: Optional[str]
+    tagList: Optional[list[str]]
 
 
-class GetTags(BaseModel):
-    tags: list[str]
-
-    class Config:
-        orm_mode = True
+class MultipleArticlesInResponse(BaseModel):
+    articles: list[ArticleForResponse]
+    articlesCount: int
