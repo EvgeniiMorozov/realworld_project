bcbd8a9122f437d5a2199f9acfa7eee38a53ac06
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-02-16 14:32:51 +0500

    Added pydantic schemas for User model

--- services/backend/src/schemas/user.py
+++ services/backend/src/schemas/user.py
@@ -1,74 +1,38 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, HttpUrl
+from pydantic import BaseModel, EmailStr, Field, SecretStr
 
 
-class LoginUser(BaseModel):
-    email: EmailStr
-    password: str
+class UserBase(BaseModel):
+    email: Optional[EmailStr] = Field(None, example="example@example.ru")
+    username: Optional[str] = Field(None, example="some_nickname")
+    bio: Optional[str] = None
+    image: Optional[str] = None
 
 
-class NewUser(LoginUser):
-    username: str
+class UserDB(UserBase):
+    id: int
+    password_hash: str
 
 
-class LoginUserRequest(BaseModel):
-    user: LoginUser
+class UserCreate(UserBase):
+    email: EmailStr = Field(..., example="example@example.ru")
+    username: str = Field(..., example="some_nickname")
+    password: SecretStr = Field(..., example="change_password")
 
 
-class NewUserRequest(BaseModel):
-    user: NewUser
-
-
-class User(LoginUser):
-    token: str
-    username: str
-    bio: str
-    image: HttpUrl
-
-    class Config:
-        orm_mode = True
-
-
-class UserInResponse(BaseModel):
-    email: EmailStr
-    token: str
-    username: str
-    bio: str
-    image: HttpUrl
-
-    class Config:
-        orm_mode = True
+class UserWithToken(UserBase):
+    token: str = Field(...)
 
 
 class UserResponse(BaseModel):
-    user: UserInResponse
+    user: UserWithToken
 
 
-class UpdateUser(BaseModel):
-    email: Optional[EmailStr] = None
-    token: Optional[str] = None
-    username: Optional[str] = None
-    bio: Optional[str] = None
-    image: Optional[HttpUrl] = None
-
-    class Config:
-        orm_mode = True
+class LoginUser(BaseModel):
+    email: str
+    password: str
 
 
-class UpdateUserRequest(BaseModel):
-    user: Optional[UpdateUser] = None
-
-
-class ProfileUser(BaseModel):
-    username: str
-    bio: str
-    image: HttpUrl
-    following: Optional[bool] = False
-
-    class Config:
-        orm_mode = True
-
-
-class ProfileUserResponse(BaseModel):
-    profile: ProfileUser
+class UserUpdate(UserBase):
+    password: Optional[str] = Field(None, example="change_password")
