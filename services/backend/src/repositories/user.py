62df96e6809488f1b67fae5e25a6e1517c88f9db
EvgeniiMorozov <75479639+EvgeniiMorozov@users.noreply.github.com>
2022-02-17 16:41:54 +0500

    Change method - create

--- services/backend/src/repositories/user.py
+++ services/backend/src/repositories/user.py
@@ -1,27 +1,25 @@
-import asyncio
-import datetime
 from typing import Optional
 
 import src.db as db
 import src.schemas as schemas
 from base import BaseRepository
-from pydantic import EmailStr, SecretStr
+from pydantic import EmailStr
 from sqlalchemy import select
 from src.core.security import get_password_hash
 
 
 class UserRepository(BaseRepository):
-    async def create(self, payload: schemas.UserCreate) -> Optional[int]:
+    async def create(self, payload: schemas.UserCreate) -> None:
         new_user = db.User(
             username=payload.username,
             email=payload.email,
             hashed_password=get_password_hash(payload.password),
         )
-        result = await self.db_session.execute(new_user)
-        return result.scalars().first()
+        self.db_session.add(new_user)
+        await self.db_session.commit()
 
     async def get(self, user_id: int) -> Optional[schemas.UserDB]:
-        query = select(db.User).where(db.User.c.id == user_id)
+        query = select(db.User).where(db.User.id == user_id)
         result = await self.db_session.execute(query)
         await self.db_session.flush()
         return schemas.UserDB(**result.scalars().first()) if result else None
